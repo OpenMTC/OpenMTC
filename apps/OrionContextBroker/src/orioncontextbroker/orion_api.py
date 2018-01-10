d3f33aa1536e9c1039264d2b2f2045d776447745
Ronald Steinke <ronald.steinke@fokus.fraunhofer.de>
2018-01-10 13:54:52 +0100

    reqrites Orion CB AE to use new ResourceManagementXAE class

--- apps/OrionContextBroker/src/orioncontextbroker/orion_api.py
+++ apps/OrionContextBroker/src/orioncontextbroker/orion_api.py
@@ -1,69 +1,48 @@
-"""
-Copyright (c) 2017 Fraunhofer FOKUS
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+try:
+    from urllib.parse import urljoin
+except ImportError:
+    from urlparse import urljoin
 
-	http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-
-import json
-from urlparse import urljoin
 import requests
-from copy import deepcopy as deepcopy
 
-from futile.logging import get_logger
-logger = get_logger(__name__)
+from futile.logging import LoggerMixin
 
 
-# TODO: check if this is sufficient
-def _isSenML(senml_dict):
-    is_senml = True
-    try:
-        is_senml = is_senml and ("bn") in senml_dict.keys()
-        is_senml = is_senml and ("n") in senml_dict.keys()
-        is_senml = is_senml and ("u") in senml_dict.keys()
-        is_senml = is_senml and ("v") in senml_dict.keys()
-        is_senml = is_senml and ("t") in senml_dict.keys()
-    except BaseException:
-        return False
-    return is_senml
-
-
-def _get_type(element):
-    if isinstance(element, int):
-        return u"Int"
-    elif isinstance(element, float):
-        return u"Float"
-    elif isinstance(element, bool):
-        return u"Boolean"
-    elif isinstance(element, (str, unicode)):
-        return u"String"
-    else:
-        logger.error("Type of \"element\" unknown")
-        return "Unknown"
-
-
-class OrionAPI:
+class OrionAPI(LoggerMixin):
 
     def __init__(self, orion_host=None, api_version="v2"):
+        super(OrionAPI, self).__init__()
         self.host = orion_host
         self.version = api_version
 
-    def create_entity(self, entitiy_name, entity_type="openmtc",
-                      fiware_service=""):
+    # TODO: check if this is sufficient
+    @staticmethod
+    def _is_senml(senml_dict):
+        try:
+            return (all(key in senml_dict for key in ('bn', 'n', 'u', 't')) and
+                    any(key in senml_dict for key in ('v', 'vs', 'vb')))
+        except TypeError:
+            return False
 
-        payload_json = {"id": entitiy_name, "type": entity_type}
+    def _get_type(self, element):
+        if isinstance(element, int):
+            return u"Int"
+        elif isinstance(element, float):
+            return u"Float"
+        elif isinstance(element, bool):
+            return u"Boolean"
+        elif isinstance(element, (str, unicode)):
+            return u"String"
+        else:
+            self.logger.error("Type of \"element\" unknown")
+            return "Unknown"
+
+    def create_entity(self, entity_name, entity_type="openmtc", fiware_service=""):
+        payload_json = {"id": entity_name, "type": entity_type}
 
         if self.version == "v2":
-            logger.debug("Send Payload to Orion CB: %s", str(payload_json))
-            response = self._request(
+            self.logger.debug("Send Payload to Orion CB: %s", str(payload_json))
+            self._request(
                 self.host + "/v2/entities",
                 method="post",
                 json=payload_json,
@@ -72,25 +51,24 @@ class OrionAPI:
                     "Content-type": "application/json",
                     "fiware-service": fiware_service}
             )
-            logger.debug("Send Payload to Orion CB: %s", str(payload_json))
+            self.logger.debug("Send Payload to Orion CB: %s", str(payload_json))
         else:
-            logger.error("API version \"%s\" not supported!", self.version)
+            self.logger.error("API version \"%s\" not supported!", self.version)
 
     def update_attributes(self, entity_id, data_senml, fiware_service=""):
-        data_senml = data_senml[0]
-        if not _isSenML(data_senml):
-            logger.error("Data \"%s\" is not valid SenML", data_senml)
+        if not self._is_senml(data_senml):
+            self.logger.error("Data \"%s\" is not valid SenML", data_senml)
             return
 
         if data_senml["v"] == "type" or data_senml["v"] == "id":
-            logger.warn(
+            self.logger.warn(
                 "SenML[v]=%s contains reserved name. Adding underscore", data_senml["v"])
             data_senml["v"] = data_senml["v"] + "_"
 
         payload_json = {
             data_senml["n"]: {
                 "value": data_senml["v"],
-                "type": _get_type(data_senml["v"]),
+                "type": self._get_type(data_senml["v"]),
                 "metadata": {
                     "timestamp": {"value": data_senml["t"], "type": "String"},
                     "bn": {"value": data_senml["bn"], "type": "String"},
@@ -100,7 +78,7 @@ class OrionAPI:
         }
 
         if self.version == "v2":
-            response = self._request(
+            self._request(
                 self.host + "/v2/entities/" + entity_id + "/attrs",
                 method="post",
                 json=payload_json,
@@ -109,29 +87,21 @@ class OrionAPI:
                     "Content-type": "application/json",
                     "fiware-service": fiware_service}
             )
-            logger.debug("Send Payload to Orion CB: %s", str(payload_json))
+            self.logger.debug("Send Payload to Orion CB: %s", str(payload_json))
         else:
-            logger.error("API version \"%s\" not supported!", self.version)
+            self.logger.error("API version \"%s\" not supported!", self.version)
 
-    def _request(
-            self,
-            url,
-            method='get',
-            json=None,
-            params=None,
-            headers=None,
-            raw=False):
-
+    def _request(self, url, method='get', json=None, params=None, headers=None, raw=False):
         joined_url = urljoin(self.host, url)
         try:
             req = requests.request(method, joined_url, json=json,
                                    params=params, headers=headers)
-            logger.debug("Status Code: %s", req.status_code)
-            logger.debug("Content: %s", req.content)
+            self.logger.debug("Status Code: %s", req.status_code)
+            self.logger.debug("Content: %s", req.content)
             if raw:
                 return {"status": req.status_code, "content": req.content}
             else:
                 return {"status": req.status_code, "content": req.json()}
         except requests.ConnectionError as e:
-            print "Connection Error: " + str(e)
+            self.logger.error("Connection Error: " + str(e))
             return {"status": -1, "content": None}
